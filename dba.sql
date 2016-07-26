dd6b282ff296f75b2a7cb693305755fb8baf0e5a
Piotr Schuetz <piotr.schuetz@gmail.com>
2016-07-26 14:44:17 +0200

    sum all datafiles

--- dba.sql
+++ dba.sql
@@ -1,4 +1,8 @@
+--show what is going on in the database
 SELECT sess.process, sess.status, sess.username, sess.schemaname, sql.sql_text
   FROM v$session sess,
        v$sql     sql
  WHERE sql.sql_id(+) = sess.sql_id
+ 
+--sum all datafiles used by databases
+select  sum(bytes)/1024/1024/1024 as GB, tablespace_name from dba_data_files group by tablespace_name
